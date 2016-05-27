16a18eabf405abc4bc7e7175eb628a8d6d18f43c
toastermagic <toastermagic@gmail.com>
2016-05-27 06:24:37 +0100

    removed environment ref

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
-import {enableProdMode} from '@angular/core';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {bootstrap} from '@angular/platform-browser-dynamic';
 import {ROUTER_PROVIDERS} from '@angular/router';
 import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material';
 
-import {HomeComponent, environment} from './app/';
+import {HomeComponent} from './app/';
 import {PlaqueService} from './app/shared';
-
-if (environment.production) {
-  enableProdMode();
-}
 
 bootstrap(
     HomeComponent,
